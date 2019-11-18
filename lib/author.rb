a3cf8f15fd5fc1934852ff2795441722cf00471a
Burbage Stacey <rstacey06@gmail.com>
2019-11-18 16:46:43 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,13 @@
+class Author 
+  
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end
+  
+  
+end
---
+++ lib/post.rb
@@ -0,0 +1,14 @@
+class Posts 
+  attr_accessor :title, :author
+
+@@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end
+end 
